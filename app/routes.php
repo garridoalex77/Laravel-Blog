ca571a8d02aa9dbaf2f888a348b37b948b70a1e7
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-06-01 10:17:39 -0500

    fix env template and added routes for resume and portfolio

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,13 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/resume', function()
+{
+    return 'Resume';
+});
+
+Route::get('/portfolio', function()
+{
+    return 'Portfolio';
+});
